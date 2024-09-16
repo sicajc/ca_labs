1f9bcc6f0eedc513928ce4eb94e8d147f1003e6b
root <root@DESKTOP-0DMQINA>
2024-09-16 16:24:07 +0800

    Merging

--- lab1/src/pipe.c
+++ lab1/src/pipe.c
@@ -160,7 +160,7 @@ void pipe_stage_mem()
     // Need to add the stall logic here and add the D$ stall cycles, if stall simply returns.
     // if(pipe.d_cache_stall > 0)
     // {
-        // pipe.d_cache_stall--;
+    // pipe.d_cache_stall--;
     // }
 
     /* if there is no instruction in this pipeline stage, we are done */
@@ -174,7 +174,7 @@ void pipe_stage_mem()
 
     uint32_t val = 0;
     // Changes this to reads the byte from D$
-    // if (op->is_mem)
+    if (op->is_mem)
         val = mem_read_32(op->mem_addr & ~3);
     //===================================================================================================
     // if (op->is_mem)
@@ -318,7 +318,7 @@ void pipe_stage_mem()
     }
 
     // if(pipe.d_cache_stall > 0)
-        // return;
+    // return;
 
     /* clear stage input and transfer to next stage */
     // Act as the output register
@@ -802,12 +802,21 @@ void pipe_stage_decode()
 
 void pipe_stage_fetch()
 {
-    if (pipe.i_cache_stall > 0)
-        pipe.i_cache_stall--;
-
     /* if pipeline is stalled (our output slot is not empty), return */
     if (pipe.decode_op != NULL)
         return;
+
+    if (RUN_BIT == 0)
+    {
+        pipe.PC += 4;
+        return;
+    }
+
+    if (pipe.i_cache_stall > 0)
+    {
+        pipe.i_cache_stall--;
+        return;
+    }
 
     // This should be replaced with interaction wth I-Cache & add the possible stall commands
     // The instruction after this stage is not stalled
@@ -817,21 +826,16 @@ void pipe_stage_fetch()
     pipe.i_cache_stall = i_cache_returned_data.cycle_time;
     //================================================================================================
 
-    printf("pc_addr: %08x\n", pipe.PC);
-
+    // Stalling
     if (pipe.i_cache_stall > 0)
     {
-        if(RUN_BIT == 0){
-            stat_inst_fetch++;
-            pipe.PC +=4;
-        }
-
         return;
     }
 
     /* Allocate an op and send it down the pipeline. */
     Pipe_Op *op = (Pipe_Op *)malloc(sizeof(Pipe_Op));
     memset(op, 0, sizeof(Pipe_Op));
+
     op->instruction = i_cache_returned_data.value;
 
     // First sets the reg_srcs all to -1
